ce0052ceedda1612c31fddf5f64620c1037a90a2
Cavs <miguelferreiranoss@gmail.com>
2026-08-30 02:20:57 +0100

    Build 43: add JitPack for offline llama.cpp Android runtime

--- android-gateway/settings.gradle.kts
+++ android-gateway/settings.gradle.kts
@@ -1,7 +1,11 @@
 pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
 }
 rootProject.name = "SDVoiceGateway"
 include(":app")
